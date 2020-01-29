ee8ade2e12acb008e761ef6f9fe744f2d9cde37a
Gabriel Song <gabsong4@gmail.com>
2020-01-29 08:36:00 -0800

    Delete trial tests and add a snapshot

--- client/src/__tests__/index.test.js
+++ client/src/__tests__/index.test.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+import toJson from 'enzyme-to-json'; // for snapshotting the component
 import Reserve from '../Reserve.jsx';
 
-it('renders without crashing', () => {
-  shallow(<Reserve />);
-});
+describe('<Reserve />', () => {
+  let wrapper;
 
-test('it works', () => {
-  expect(1).toBe(1);
-});
+  it('renders without crashing', () => {
+    wrapper = shallow(<Reserve />);
+  });
 
-test('arithmetic', () => {
-  expect(4 + 4).toBeGreaterThan(7);
-  expect(4 + 4).toBeLessThan(9);
-});
-
-test('references', () => {
-  const arr = [1, 2, 3];
-  expect(arr).toEqual([1, 2, 3]);
-  expect(arr).not.toBe([1, 2, 3]); // since === doesn't do deep comparison!
-  expect(arr).toContain(1);
+  it('matches snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
