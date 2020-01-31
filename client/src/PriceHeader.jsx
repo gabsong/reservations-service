842f9983f6985721e3b92cceecf2ceb6cefc83a9
Gabriel Song <gabsong4@gmail.com>
2020-01-31 11:22:25 -0800

    Do exact CSS styling

--- client/src/PriceHeader.jsx
+++ client/src/PriceHeader.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/extensions */
 import React from 'react';
+import styles from './PriceHeader.module.css';
 
 const PriceHeader = (props) => {
   const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
@@ -8,10 +9,12 @@ const PriceHeader = (props) => {
   return (
     <div>
       <div className="price-info">
-        ${props.nightlyRate} per night
+        <span className={styles.price}>${props.nightlyRate}</span> <span className={styles.type}>per night</span>
       </div>
       <div className="review-info">
-        * {'4.75'} ({200} reviews)
+        <span className={styles.star} />
+        <span className={styles.rating}> {'4.75'} </span>
+        <span className={styles.reviews}>({200} reviews)</span>
       </div>
     </div>
   );
